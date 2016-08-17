4a5f959be7a9c855db55448d44788ab81c334077
Lukas Keilholz <lukas.keilholz@web.de>
2016-08-17 20:27:46 +0200

    - datastucture developed

---
+++ Webcrawler_GetInIT/DBAccess/DatabaseAccess.vb
@@ -0,0 +1,37 @@
+﻿
+Public Class DatabaseAccess
+
+    Private ReadOnly connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=(local);Persist Security Info=False;"
+    Private connection As OleDb.OleDbConnection
+
+    Public Sub New()
+
+        Dim conn As OleDb.OleDbConnection = getConnection()
+        conn.Open()
+
+        Dim cmd As New OleDb.OleDbCommand("CREATE DATABASE IF NOT EXISTS MyDb", conn)
+        cmd.ExecuteNonQuery()
+
+        cmd.CommandText = "CREATE TABLE IF NOT EXISTS JobOffer(Id INTEGER PRIMARY KEY NOT NULL, OfferTitle VARCHAR(100), Company VARCHAR(100), CoreAreas VARCHAR(100))
+                           FieldsOfStudy VARCHAR(100), Degrees VARCHAR(100), Locations VARCHAR(150), NiceToKnow VARCHAR(2000), Description VARCHAR(10000),
+                           ULR VARCHAR(100), HTML VARCHAR(200000))"
+        cmd.ExecuteNonQuery()
+
+    End Sub
+
+    Public Sub AddJobOffers(jobs As List(Of JobOffer))
+
+        'TODO
+
+
+    End Sub
+
+    Private Function getConnection() As OleDb.OleDbConnection
+        If connection Is Nothing Then
+            connection = New OleDb.OleDbConnection(connectionString)
+        End If
+        Return connection
+    End Function
+
+
+End Class
---
+++ Webcrawler_GetInIT/DBAccess/JobOffer.vb
@@ -0,0 +1,133 @@
+﻿Public Class JobOffer
+
+    Private _OfferTitle As String
+    Private _Company As String
+    Private _CoreAreas As List(Of String)
+    Private _FieldsOfStudy As List(Of String)
+    Private _Degrees As List(Of String)
+    Private _Locations As List(Of String)
+    Private _NiceToKnow As String
+    Private _Description As String
+    Private _URL As String
+    Private _HTML As String
+
+#Region "Properties"
+    Public Property OfferTitle As String
+        Get
+            Return _OfferTitle
+        End Get
+        Set(value As String)
+            _OfferTitle = value
+        End Set
+    End Property
+
+    Public Property Company As String
+        Get
+            Return _Company
+        End Get
+        Set(value As String)
+            _Company = value
+        End Set
+    End Property
+
+    Public Property CoreAreas As List(Of String)
+        Get
+            Return _CoreAreas
+        End Get
+        Set(value As List(Of String))
+            _CoreAreas = value
+        End Set
+    End Property
+
+    Public Property FieldsOfStudy As List(Of String)
+        Get
+            Return _FieldsOfStudy
+        End Get
+        Set(value As List(Of String))
+            _FieldsOfStudy = value
+        End Set
+    End Property
+
+    Public Property Degrees As List(Of String)
+        Get
+            Return _Degrees
+        End Get
+        Set(value As List(Of String))
+            _Degrees = value
+        End Set
+    End Property
+
+    Public Property Locations As List(Of String)
+        Get
+            Return _Locations
+        End Get
+        Set(value As List(Of String))
+            _Locations = value
+        End Set
+    End Property
+
+    Public Property URL As String
+        Get
+            Return _URL
+        End Get
+        Set(value As String)
+            _URL = value
+        End Set
+    End Property
+
+    Public Property NiceToKnow As String
+        Get
+            Return _NiceToKnow
+        End Get
+        Set(value As String)
+            _NiceToKnow = value
+        End Set
+    End Property
+
+    Public Property Description As String
+        Get
+            Return _Description
+        End Get
+        Set(value As String)
+            _Description = value
+        End Set
+    End Property
+
+    Property HTML As String
+        Get
+            Return _HTML
+        End Get
+        Set(value As String)
+            _HTML = value
+        End Set
+    End Property
+
+    '
+    'End of property section
+    '=========================================================================================================================================================
+#End Region
+
+    Public Sub New()
+        Me.CoreAreas = New List(Of String)
+        Me.FieldsOfStudy = New List(Of String)
+        Me.Degrees = New List(Of String)
+        Me.Locations = New List(Of String)
+    End Sub
+
+
+
+    Public Sub New(OfferTitle As String, Company As String, CoreAreas As List(Of String), FieldsOfStudy As List(Of String),
+                   Degrees As List(Of String), Locations As List(Of String), NiceToKnow As String, Description As String, URL As String, HTML As String)
+        Me.OfferTitle = OfferTitle
+        Me.Company = Company
+        Me.CoreAreas = CoreAreas
+        Me.FieldsOfStudy = FieldsOfStudy
+        Me.Degrees = Degrees
+        Me.Locations = Locations
+        Me.NiceToKnow = NiceToKnow
+        Me.Description = Description
+        Me.URL = URL
+        Me.HTML = HTML
+    End Sub
+
+End Class
